7578609c2a9d23ba05b0cdb54103fdbcdcdd5839
Alisher Umarov <badmaddoppelganger@gmail.com>
2022-08-14 02:28:06 -0700

    Release final version of hw1 after refactoring and perfomance test

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,50 +4,48 @@
 """
 
 
-def power_numbers(*args):
+def power_numbers(*args: int) -> list:
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     """
-    up_sqrt = [i ** 2 for i in args]
-    return up_sqrt
+    return [i ** 2 for i in args]
 
 
-def is_prime(digit):
-    """method that return that the digit is prime or not
+def is_prime(digit: int):
+    """method return that the digit is prime or not
     """
     if digit in [0, 1]:
         return False
-    for a in range(2, digit):
-        if (digit % a) == 0:
+#    return not any(num for num in range(2, int(digit / 2) + 1) if digit % num == 0)
+    for num in range(2, int(digit / 2) + 1):
+        if (digit % num) == 0:
             return False
-    else:
-        return True
+    return True
 
 
-def is_odd(digit):
-    """method that return that the digit is odd or not
+def is_odd(digit: int) -> int:
+    """method return that the digit is odd or not
     """
-    return digit % 2 == 1
+    return digit & 1
 
 
-def is_even(digit):
-    """method that return that the digit is odd or not
+def is_even(digit: int) -> bool:
+    """method return that the digit is odd or not
     """
-    return digit % 2 == 0
+    return not digit & 1
 
 
-# filter types
+# filter types (PEP8 E731 do not assign a lambda expression, use a def!)
 ODD = is_odd
 EVEN = is_even
 PRIME = is_prime
 
 
-def filter_numbers(lst, func):
+def filter_numbers(lst: list[int], func) -> list:
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
     """
-    ls = filter(func, lst)
-    return list(ls)
+    return list(filter(func, lst))
